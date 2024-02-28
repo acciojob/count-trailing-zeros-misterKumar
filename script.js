95d766b8a5e4d35e6a18cc1a63fc0e0e300a0573
misterKumar <120304503+misterKumar@users.noreply.github.com>
2024-02-28 20:24:41 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,15 +1,16 @@
 function trailingZeros(n) {
   //your JS code here. If required.
-	let sum=1;
-	for(int i=1;i<=n;i++){
-		sum*=i;
-	}
-	let c=0;
-	while(sum%10===0){
-		sum/=10;
-		c++;
-	}
-	return c;
+	if(n < 0) //Negative Number Edge Case
+      return -1;
+    // Initialize result
+    let count = 0;
+
+    // Keep dividing n by powers of
+    // 5 and update count
+    for (let i = 5; Math.floor(n / i) >= 1; i *= 5)
+        count += Math.floor(n / i);
+ 
+    return count;
 }
 
 const input = prompt("Enter a number");
