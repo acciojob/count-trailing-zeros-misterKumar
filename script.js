a17ce38c306bd9253cb201f0f943958c0bd83266
misterKumar <120304503+misterKumar@users.noreply.github.com>
2024-02-28 20:20:25 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,15 @@
 function trailingZeros(n) {
   //your JS code here. If required.
+	let sum=1;
+	for(int i=1;i<=n;i++){
+		sum*=i;
+	}
+	let c=0;
+	while(sum%10===0){
+		sum/=10;
+		c++;
+	}
+	return c;
 }
 
 const input = prompt("Enter a number");
